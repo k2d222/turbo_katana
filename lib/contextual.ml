33a7701a49562145cbb0055626de922bdd4aa032
mathis <mathis.brossier@universite-paris-saclay.fr>
2021-12-31 12:40:46 +0100

    organize order of args

--- lib/contextual.ml
+++ lib/contextual.ml
@@ -4,50 +4,42 @@ exception Contextual_error of string
 
 (** Find the class declaration with a given name. *)
 
-let find_class_opt name decls =
+let find_class_opt decls name =
   List.find_opt (fun decl -> decl.name = name) decls
 
 (** Find the class declaration with a given name.
     @raise Not_found if no such declaration is found. *)
 
-let find_class name decls =
+let find_class decls name =
   List.find (fun decl -> decl.name = name) decls
 
 (** List of all ancestor class declarations in bottom-to-top order.
     @raise Not_found if an ancestor has no declaration. *)
 
-let rec ancestors decl decls =
+let rec ancestors decls decl =
   match decl.superclass with
   | None -> []
   | Some(super) ->
-    let superDecl = (find_class super decls) in
-    superDecl :: (ancestors superDecl decls)
+    let superDecl = (find_class decls super) in
+    superDecl :: (ancestors decls superDecl)
 
 (** Find (recursively through ancestors) the method declaration in a class
     with a given name. *)
 
-let rec find_method_opt decl name decls =
+let rec find_method_opt decls name decl =
   List.find_opt (fun (meth: methodDecl) -> meth.name = name) decl.body.methods
   |> Optmanip.or_else (fun () ->
       match decl.superclass with
       | None -> None
       | Some(super) ->
-        let superDecl = (find_class super decls)
-        in find_method_opt superDecl name decls
+        let superDecl = (find_class decls super)
+        in find_method_opt decls name superDecl
     )
-
-(** Tells if a class extends a base class (recursively)
-    @raise Not_found if a class name has no declaration. *)
-
-let is_superclass base super decls =
-  let superDecl = find_class super decls in
-  List.find_opt ((=) base) (ancestors superDecl decls)
-  |> Option.is_some
 
 
 (** Get the type of an attribute in a class declaration. *)
 
-let get_attr_type decl attrName =
+let get_attr_type attrName decl =
   decl.body.instAttrs
   |> List.find_map  (fun (attr: param) ->
       if attr.name = attrName  then Some(attr.className)  else None
@@ -56,7 +48,7 @@ let get_attr_type decl attrName =
 
 (** Get the type of a method in a class declaration. *)
 
-let get_method_type decl methName =
+let get_method_type methName decl =
   decl.body.methods
   |> List.find_map  (fun (meth: methodDecl) ->
       if meth.name = methName then meth.retType else None
@@ -65,7 +57,7 @@ let get_method_type decl methName =
 
 (** Computes an expression type. *)
 
-let get_expr_type env decls expr =
+let get_expr_type decls env expr =
   let rec r_get expr =
     match expr with
     | Cste _ | BinOp _ | UMinus _ -> "Integer"
@@ -74,28 +66,37 @@ let get_expr_type env decls expr =
     | Id id -> List.assoc id env
 
     | Attr(e, attrName) ->
-      let decl = find_class (r_get e) decls
-      in get_attr_type decl attrName
+      let decl = find_class decls (r_get e)
+      in get_attr_type attrName decl
 
     | StaticAttr(className, attrName) ->
-      let decl = find_class className decls
-      in get_attr_type decl attrName
+      let decl = find_class decls className
+      in get_attr_type attrName decl
 
     | List l ->
       let last = List.hd (List.rev l)
       in r_get last
 
     | Call(caller, name, _args) ->
-      let decl = find_class (r_get caller) decls
-      in get_method_type decl name
+      let decl = find_class decls (r_get caller)
+      in get_method_type name decl
 
     | StaticCall(className, name, _args) ->
-      let decl = find_class className decls
-      in get_method_type decl name
+      let decl = find_class decls className
+      in get_method_type name decl
 
     | New(className, _args) -> className
 
   in r_get expr
+
+(** Wether derived is convertible to base. *)
+
+let is_base decls derived base =
+  if derived = base then true
+  else
+    let derived = find_class decls derived
+    in let base = find_class decls base
+    in List.exists ((=) base) (ancestors decls derived)
 
 (* -------------------------------------------------------------------------- *)
 
@@ -140,7 +141,7 @@ let check_multiple_def decl =
 let check_inheritance decls =
   List.filter_map (fun d -> d.superclass |> Optmanip.map (fun super -> (d.name, super))) decls
   |> List.iter (fun (name, super) ->
-      match find_class_opt super decls with
+      match find_class_opt decls super with
       | None -> raise @@ Contextual_error (Printf.sprintf "class '%s' extends non-existing class '%s'" name super)
       | _ -> ()
     )
@@ -153,7 +154,7 @@ let check_cycles decls =
   let rec r_check ancestors decl =
     match decl.superclass with
     | Some(super) ->
-      let superDecl = find_class_opt super decls |> Option.get in
+      let superDecl = find_class_opt decls super |> Option.get in
       if List.exists ((=) super) ancestors
       then raise @@ Contextual_error (Printf.sprintf "cycle in heritance: class '%s' extends ancestor class '%s'" decl.name super)
       else r_check (super::ancestors) superDecl
@@ -171,7 +172,7 @@ let check_overrides decls =
   let check_class decl =
 
     let check_super_method superDecl (meth: methodDecl) =
-      let overriden = find_method_opt superDecl meth.name decls in
+      let overriden = find_method_opt decls meth.name superDecl in
       if meth.override then
         match overriden with
         | Some(overriden) ->
@@ -191,13 +192,13 @@ let check_overrides decls =
 
     in match decl.superclass with
     | Some(super) ->
-      let superDecl = find_class super decls
+      let superDecl = find_class decls super
       in List.iter (check_super_method superDecl) decl.body.methods
     | None -> List.iter check_base_method decl.body.methods
 
   in List.iter check_class decls
 
-(** Checks that an expression is valid with the given scope.
+(** Checks that all identifiers in an expression are in scope.
     @raise Contextual_error if a check fails. *)
 
 let rec check_expr_scope env expr =
@@ -208,13 +209,13 @@ let rec check_expr_scope env expr =
     - Method call parameters are compatible with the declaration.
       @raise Contextual_error if a check fails. *)
 
-let rec check_method_calls env expr instr =
+let rec check_method_calls _env _expr _instr =
   () (* TODO *)
 
 (** Performs the following checks:
     - Left-hand-side assign operand refers to either:
-      (a) A variable in the scope, or
-      (b) (recusively) an attribute of a variable in the scope, or
+      (a) An ident,
+      (b) (recusively) an attribute of a variable, or
       (c) A static attribute of a class.
     - Right-hand-side assign operand is compatible with the target variable
       @raise Contextual_error if a check fails. *)
@@ -226,28 +227,29 @@ let rec check_assign env (lhs, rhs) =
     - All code paths lead to either:
       (a) A return, or
       (b) an assign to the implicit 'result' variable.
-    - All return instructions have a type compatible with the return type *)
+    - All return instructions have a type compatible with the return type
+    - All assigns to result have a type compatible with the return type *)
 
 let check_returns retType instr =
-  let rec r_check env instr =
+  let rec r_check _env _instr =
     () (* TODO *)
   in r_check [("result", retType)] instr
 
 (** Checks that there are no return instruction.
     @raise Contextual_error if a check fails. *)
 
-let rec check_no_return instr =
+let rec check_no_return _instr =
   () (* TODO *)
 
 (** Checks that an if/then/else instruction is valid.
     @raise Contextual_error if a check fails. *)
 
-let check_ite_instr (if_, then_, else_) =
+let check_ite_instr (_if_, _then_, _else_) =
   () (* TODO *)
 
 (* -------------------------------------------------------------------------- *)
 
-let check_main_instr instr decls =
+let check_main_instr _decls instr =
   check_no_return instr;
   () (* TODO *)
 
@@ -268,4 +270,4 @@ let check_decls decls =
 
 let check_all ast =
   check_decls ast.decls;
-  check_main_instr ast.instr ast.decls
+  check_main_instr ast.decls ast.instr
