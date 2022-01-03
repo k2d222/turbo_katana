6e97d95a889b7ac289c188f078ea50ecf2aba298
mathis <mathis.brossier@universite-paris-saclay.fr>
2022-01-03 19:00:54 +0100

    @@ too much

--- test/test.ml
+++ test/test.ml
@@ -8,64 +8,64 @@ let parse_file path =
   In_channel.close file;
   ast
 
-let parse_str str = 
+let parse_str str =
   let lexbuf = Lexing.from_string str
   in Parser.prog Lexer.token lexbuf
 
-let expects_err f = 
-  try ignore @@ f (); false 
+let expects_err f =
+  try ignore @@ f (); false
   with Failure _ -> true
 
-let expects_parse_err f = 
-  try ignore @@ f (); false 
+let expects_parse_err f =
+  try ignore @@ f (); false
   with Parser.Error -> true
 
-let expects_ctx_err f = 
-  try ignore @@ f (); false 
+let expects_ctx_err f =
+  try ignore @@ f (); false
   with Contextual.Contextual_error _ -> true
 
 let expects_ast f =
-  try ignore @@ f (); true with Parser.Error -> false  
+  try ignore @@ f (); true with Parser.Error -> false
 
 (* ------------------------------------- *)
 
 
 let%test "ex1-parse" =
-  expects_ast @@ (fun () ->
-    parse_file "../progs/ex1.kat"
-  )
-  
+  expects_ast (fun () ->
+      parse_file "../progs/ex1.kat"
+    )
+
 (* ---------- Lexical tests ---------- *)
 
 let%test "invalid-token" =
-  expects_err @@ (fun () -> 
-    parse_str {| # |}
-  )
+  expects_err (fun () ->
+      parse_str {| # |}
+    )
 
 (* ---------- Parsing tests ---------- *)
 
 let%test "empty-prog" =
-  expects_parse_err @@ (fun () -> 
-    parse_str {| |}
-  )
+  expects_parse_err (fun () ->
+      parse_str {| |}
+    )
 
 (* ---------- Contextual tests ---------- *)
 
 let%test "minimal-prog" =
-  expects_ast @@ (fun () -> 
-    parse_str {| {} |}
-  )
+  expects_ast (fun () ->
+      parse_str {| {} |}
+    )
 
 let%test "class-inherits-string" =
-  expects_ctx_err @@ (fun () -> 
-    Contextual.check_all @@ parse_str {|
+  expects_ctx_err (fun () ->
+      Contextual.check_all @@ parse_str {|
       class Test() extends String is {
         def Test() is {}
       }
       {}
     |}
-  )
-  
+    )
+
 (* ---------- Compilation tests ---------- *)
 
 
