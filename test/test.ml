a107d31d3228323f6bf7fd3dd003583214d96ffa
mathis <mathis.brossier@universite-paris-saclay.fr>
2022-01-04 12:25:49 +0100

    better testing

--- test/test.ml
+++ test/test.ml
@@ -12,59 +12,58 @@ let parse_str str =
   let lexbuf = Lexing.from_string str
   in Parser.prog Lexer.token lexbuf
 
-let expects_err f =
-  try ignore @@ f (); false
+let expects_err str =
+  try Contextual.check_all @@ parse_str str; false
   with Failure _ -> true
 
-let expects_parse_err f =
-  try ignore @@ f (); false
+let expects_parse_err str =
+  try Contextual.check_all @@ parse_str str; false
   with Parser.Error -> true
 
-let expects_ctx_err f =
-  try ignore @@ f (); false
+let expects_ctx_err str =
+  try Contextual.check_all @@ parse_str str; false
   with Contextual.Contextual_error _ -> true
 
-let expects_ast f =
-  try ignore @@ f (); true with Parser.Error -> false
+let expects_ast str =
+  Contextual.check_all @@ parse_str str; true
 
 (* ------------------------------------- *)
 
 
-let%test "ex1-parse" =
-  expects_ast (fun () ->
-      parse_file "../progs/ex1.kat"
-    )
+let%test_unit "ex1-parse" =
+    ignore @@ parse_file "../progs/ex1.kat"
 
 (* ---------- Lexical tests ---------- *)
 
 let%test "invalid-token" =
-  expects_err (fun () ->
-      parse_str {| # |}
-    )
+  expects_err {| # |}
 
 (* ---------- Parsing tests ---------- *)
 
 let%test "empty-prog" =
-  expects_parse_err (fun () ->
-      parse_str {| |}
-    )
+  expects_parse_err {| |}
 
 (* ---------- Contextual tests ---------- *)
 
 let%test "minimal-prog" =
-  expects_ast (fun () ->
-      parse_str {| {} |}
-    )
+  expects_ast {| {} |}
 
-let%test "class-inherits-string" =
-  expects_ctx_err (fun () ->
-      Contextual.check_all @@ parse_str {|
-      class Test() extends String is {
+let%test "no-class-inherits-reserved" =
+  let reserved = [ "Integer"; "String"; "Void" ]
+  in let code = Printf.sprintf {|
+      class Test() extends %s is { def Test() is {} }
+      {}
+    |}
+  in List.for_all reserved ~f:(fun r -> expects_ctx_err (code r))
+
+let%test "no-static-override" =
+  expects_ctx_err {|
+      class Test() is {
         def Test() is {}
+        def static override foo() is {}
       }
       {}
     |}
-    )
 
 (* ---------- Compilation tests ---------- *)
 
