90aa6bcc9ecdd78861cc9222b241648d4bd97400
Pierre Surer <pierre.surer@u-psud.fr>
2022-01-09 20:27:03 +0100

    contextual tests (wip)

--- doc/contextual_checks.md
+++ doc/contextual_checks.md
@@ -5,7 +5,7 @@
 * Perform Constructor checks
 * Perform Methods checks
 * --
-* No class derived from Integer or String
+* No class derived from Integer or String ✔
 * No duplicate class declaration
 * No reserved class name (Integer, String)
 * No duplicate static attribute declaration
@@ -19,18 +19,19 @@
 
 * Perform *Instruction* checks on method body
 * --
-* No reserved keyword in params
+* No reserved keyword in params ✔
 * No method with *override* keyword in a base class
 * Override methods have the *override* keyword
 * Override methods match the overriden method signature
 * If method returns something, all code paths lead to a return or assign to *result*
 * If methods returns something, all Return instructions are compatible with return type
 * If method returns nothing, no Return instruction
+* No static override ✔
 
 ## Constructor
 
 * No Return instruction
-* No reserved keyword in params
+* No reserved keyword in params ✔
 * Constructor name and class name are equal
 * Constructor parameters and class parameters are equal
 * Constructor calls the right super constructor if class is derived
--- test/context.ml
+++ test/context.ml
@@ -22,3 +22,66 @@ let%test "no-static-override" =
       }
       {}
     |}
+
+let%test "underclared_variable" =
+  expects_ctx_err {|
+      {p1 := 51;}
+    |}
+  
+let%test "unmatched-type" =
+  expects_ctx_err {|
+      class Point1() is {
+        def Point1() is {}
+      }
+
+      class Point2() is {
+        def Point2() is {}
+      }
+
+      {
+        p1: Point1
+        p2: Point2
+        is
+        p1 := p2;
+      }
+    |}
+
+let%test "no-reserved-keyword-in-constructor-params" = 
+  let reserved = [ "this"; "super"; "result" ]
+  in let code = Printf.sprintf {|
+      class Test(%s : Integer) is { 
+        def Test(%s : Integer) is {}
+      }
+      {}
+    |}
+  in List.for_all reserved ~f:(fun r -> expects_ctx_err (code r r))
+
+let%test "no-reserved-keyword-in-method-params" = 
+  let reserved = [ "this"; "super"; "result" ]
+  in let code = Printf.sprintf {|
+      class Test() is { 
+        def Test() is {}
+        def testMethod(%s : Integer) : Integer is {}
+      }
+      {}
+    |}
+  in List.for_all reserved ~f:(fun r -> expects_ctx_err (code r))
+
+let%test "no-reserved-keyword-in-attributes" = 
+  let reserved = [ "this"; "super"; "result" ]
+  in let code = Printf.sprintf {|
+      class Test() is { 
+        def Test() is {}
+        var %s : Integer;
+      }
+      {}
+    |}
+  in List.for_all reserved ~f:(fun r -> expects_ctx_err (code r))
+
+let%test "no-reserved-keyword-in-instructions" = 
+  let reserved = [ "this"; "super"; "result" ]
+  in let code = Printf.sprintf {|
+      {%s : Integer is {}}
+    |}
+  in List.for_all reserved ~f:(fun r -> expects_ctx_err (code r))
+
